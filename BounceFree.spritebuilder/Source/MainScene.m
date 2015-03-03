8952cab26b6e4afd9965a00179c1c64ced951458
YinLinCMU <linyin1112@gmail.com>
2015-03-03 00:24:44 -0800

    Automatically Committed for MakeGamesWithUs

--- BounceFree.spritebuilder/Source/MainScene.m
+++ BounceFree.spritebuilder/Source/MainScene.m
@@ -147,9 +147,10 @@
         NSUInteger b = arc4random_uniform(300);
         ghost.position = ccp(ghost.position.x, b);
         obstacle.zOrder = DrawingOrderPipes;
-        [physicsNode addChild:ghost];
+        
         NSUInteger c = arc4random_uniform(5);
         ghost.physicsBody.velocity = ccp(self.physicsBody.velocity.x, c-10);
+        [physicsNode addChild:ghost];
 
     }/*
     else{

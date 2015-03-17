480e9c4d5564492eea8c53c2534d9efb2a51bd12
YinLinCMU <linyin1112@gmail.com>
2015-03-16 20:31:06 -0700

    Automatically Committed for MakeGamesWithUs

--- BounceFree.spritebuilder/Source/Coin.h
+++ BounceFree.spritebuilder/Source/Coin.h
@@ -1,8 +1,8 @@
 //
 //  Coin.h
-//  FlappyBird
+//  BounceFree
 //
-//  Created by Yin Lin on 2/20/15.
+//  Created by Yin Lin on 2/21/15.
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
--- BounceFree.spritebuilder/Source/Coin.m
+++ BounceFree.spritebuilder/Source/Coin.m
@@ -1,19 +1,19 @@
 //
 //  Coin.m
-//  FlappyBird
+//  BounceFree
 //
-//  Created by Yin Lin on 2/20/15.
+//  Created by Yin Lin on 2/21/15.
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
 #import "Coin.h"
 
 @implementation Coin{
-     CCNode *_coin;
+    CCNode *_coin;
 }
 - (void)didLoadFromCCB {
     _coin.physicsBody.collisionType = @"bonus";
     _coin.physicsBody.sensor = YES;
-
+    
 }
 @end

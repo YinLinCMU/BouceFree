51bd63119cea6fa70927db52b3845848739f728b
YinLinCMU <linyin1112@gmail.com>
2015-03-31 14:29:39 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ BounceFree.spritebuilder/Source/Fence.h
@@ -0,0 +1,13 @@
+//
+//  Fence.h
+//  BounceFree
+//
+//  Created by Yin Lin on 3/31/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Fence : CCSprite
+
+@end

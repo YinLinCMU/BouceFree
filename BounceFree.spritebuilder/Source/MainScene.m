ed754e99aa5d8d865e1f9d4438b2cb0e386e1846
YinLinCMU <linyin1112@gmail.com>
2015-03-31 17:13:29 -0700

    Automatically Committed for MakeGamesWithUs

--- BounceFree.spritebuilder/Source/MainScene.m
+++ BounceFree.spritebuilder/Source/MainScene.m
@@ -208,7 +208,7 @@
     
     physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
        _parallaxBackground.position = ccp(_parallaxBackground.position.x - (character.physicsBody.velocity.x * delta), _parallaxBackground.position.y);
-    
+    NSLog("y %s", character.position.y);
     if (character.position.y >= 290) {
         //[self gameOver];
         //character.position = ccp(character.position.x, 21);

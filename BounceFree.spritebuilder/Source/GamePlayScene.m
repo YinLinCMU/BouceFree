c93424f48a004b2501b00ca02224f20e5353dc86
YinLinCMU <linyin1112@gmail.com>
2015-04-14 13:45:33 -0700

    Automatically Committed for MakeGamesWithUs

--- BounceFree.spritebuilder/Source/GamePlayScene.m
+++ BounceFree.spritebuilder/Source/GamePlayScene.m
@@ -52,8 +52,8 @@
 - (void)play :(NSString *)fName :(NSString *) ext{
     SystemSoundID audioEffect;
     NSString *path = [[NSBundle mainBundle] pathForResource : fName ofType :ext];
-    NSLog(@"path = %@",fName);
-    if ([[NSFileManager defaultManager] fileExistsAtPath : @"bounce.wav"]) {
+    NSLog(@"path = %@",path);
+    if ([[NSFileManager defaultManager] fileExistsAtPath : path]) {
         NSURL *pathURL = [NSURL fileURLWithPath: path];
 
         AudioServicesCreateSystemSoundID((__bridge CFURLRef) pathURL, &audioEffect);
